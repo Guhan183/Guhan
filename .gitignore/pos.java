118b1b821a7379b9c454c428e70ee4aea6688180
Guhan183 <40463310+Guhan183@users.noreply.github.com>
2018-06-21 17:48:06 +0530

    Update and rename .gitignore to pos.java

---
+++ .gitignore/pos.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+class pos
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>0)
+System.out.print("positive");
+else if(n<0)
+System.out.print("Negative");
+else
+System.out.print("Zero");
+}
+}
